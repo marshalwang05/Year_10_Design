392b41c9b15f81d898be414fd7dfbecea470800d
Marshal Wang <marshal.wang@GCCYJQBEJ1WK.local>
2020-09-21 13:57:24 -0400

    Ferengi API

---
+++ Ferengi_Currency_Relocation_Project/accessAPI.py
@@ -0,0 +1,70 @@
+import requests
+import json
+import tkinter as tk
+
+#Get Key
+#This is a file not in my respository I don't want you to have it
+file = open("..//..//API_Keys//fixerkey.txt","r")
+
+key = file.read()
+
+
+resp = requests.get('http://data.fixer.io/api/latest?access_key='+key)
+
+#Converts response to JSON
+data = resp.json()
+
+print(data["base"])
+print(data["date"])
+print(data["rates"]["CAD"])
+print(data["rates"])
+
+
+
+#Step 1: Create two lists
+country = []
+value = []
+rate_data = data["rates"]
+
+
+def action(x):
+	print(x)
+	print(rate_data[x])
+
+
+#Step 2: Copy all the data from the pull into these 
+# Loop through all rates using a "special loop"
+for key in rate_data:
+	country.append(key)
+	value.append(rate_data[key])
+
+print(country)
+print(value)
+
+root = tk.Tk() #starts  root window
+
+f1 = tk.Frame(root)
+
+cur_label = tk.Label(f1, text = "Select Currency")
+var = tk.StringVar(f1)
+var.set(country[0])
+dropDown = tk.OptionMenu(f1,var,*country, command = action)
+
+cur_label.pack()
+dropDown.pack()
+f1.pack()
+
+
+
+root.mainloop() # Starts programs and waits for instructions
+print("END")
+
+
+
+
+
+
+
+
+
+
